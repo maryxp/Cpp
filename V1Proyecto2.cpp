86465a825ecd5b0e148152a97f08c540d9432a22
Maria Laura <mary.cool2707@gmail.com>
2023-04-27 18:59:10 -0400

    segundo commit proyecto2

--- V1Proyecto2.cpp
+++ V1Proyecto2.cpp
@@ -1,36 +1,80 @@
 #include <iostream>
 using namespace std;
 
-public class Mapa{
+class Mapa{
 	enum class Elementos{
-		vacio, jugador, caja, pared
+		VACIO, JUGADOR, CAJA, PARED, FINAL
 	};
 
-	private int posJugador_x;
-	private int posJugador_y;
-	private posInicial;
-	private posFinal;
+	int posJugador_x;
+	int posJugador_y;
+	int posInicial;
+	int posFinal;
+	Elementos** matriz;		
+	//La matriz posse elementos. Elementos es un tipo. La matriz es de elementos
+	int N, M;		//El user no sabra que eso es una matriz
 
-	public Mapa(){
+	public:
 
+	Mapa(){
+		cin>>N>>M;
 	}
 
-	public Mapa(int posJugador_x, int posJugador_y, posInicial, posFinal){
-		this.posJugador_x = posJugador_x;
-		this.posJugador_y = posJugador_y;
-		this.posInicial = posInicial;
-		this.posFinal = posFinal;
+	Mapa(int posJugador_x, int posJugador_y, int posInicial, int posFinal){
+		this->posJugador_x = posJugador_x;
+		this->posJugador_y = posJugador_y;
+		this->posInicial = posInicial;
+		this->posFinal = posFinal;
 	}
-
+	
 	void mover_arriba();
 	void mover_izquierda();
 	void mover_abajo();
 	void mover_derecha();
-	void imprimir();
-}
 
-int main(int argc, char const *argv[])
-{
-	
+	/*int** crear_matriz(int N, int M){
+		int** matriz = new int* [M];
+
+		for(int i = 0; i < N; i++){
+			matriz[i] = new int[M];	//Crear columnas
+
+		}
+		return matriz;
+	}*/
+
+	void imprimir_tablero(int N, int M){
+		
+		for(int i = 1; i <= N; i++){
+			for(int j = 1; j <= M; j++){
+		
+				if (matriz[i][j] == Elementos::VACIO){
+					cout<<"."<<endl;
+				}
+
+				if (matriz[i][j] == Elementos::JUGADOR){
+					cout<<"K"<<endl;
+				}
+
+				if (matriz[i][j] == Elementos::CAJA){
+					cout<<"C"<<endl;
+				}
+
+				if (matriz[i][j] == Elementos::PARED){
+					cout<<"X"<<endl;
+				}
+
+				if (matriz[i][j] == Elementos::FINAL){
+					cout<<"F"<<endl;
+				}
+
+			}
+		}
+	}	
+};
+
+int main(){
+
+	Mapa map;			//Instancie mapa
+	map.imprimir_tablero(N, M);
 	return 0;
 }
