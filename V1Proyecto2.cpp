3c66f17999ae3f1ea3528e1895e1fb25c8735eb2
Maria Laura <mary.cool2707@gmail.com>
2023-05-02 20:54:21 -0400

    Primer tester

--- V1Proyecto2.cpp
+++ V1Proyecto2.cpp
@@ -2,29 +2,40 @@
 using namespace std;
 
 class Mapa{
+	public:			//Para que se pueda acceder desde fuera de la clase 
 	enum class Elementos{
 		VACIO, JUGADOR, CAJA, PARED, FINAL
 	};
 
+	private: 
 	int posJugador_x;
 	int posJugador_y;
-	int posInicial;
-	int posFinal;
-	Elementos** matriz;		
-	//La matriz posse elementos. Elementos es un tipo. La matriz es de elementos
-	int N, M;		//El user no sabra que eso es una matriz
-
+	int posInicial_x;
+	int posInicial_y;
+	int posFinal_x;
+	int posFinal_y;
+	
 	public:
+	int N, M;
+								//Public
+	Elementos** matriz;			//Para que se pueda usar la matriz por otras funciones
+	//La matriz posse elementos. Elementos es un tipo. La matriz es de elementos
 
 	Mapa(){
-		cin>>N>>M;
+		
 	}
 
-	Mapa(int posJugador_x, int posJugador_y, int posInicial, int posFinal){
+	Mapa(int posJugador_x, int posJugador_y, int posInicial_x, int posInicial_y, int posFinal_x, int posFinal_y, Elementos** matriz, int N, int M){
+			
 		this->posJugador_x = posJugador_x;
 		this->posJugador_y = posJugador_y;
-		this->posInicial = posInicial;
-		this->posFinal = posFinal;
+		this->posInicial_x = posInicial_x;
+		this->posInicial_y = posInicial_y;
+		this->posFinal_x = posFinal_x;
+		this->posFinal_y = posFinal_y;
+		this ->matriz = matriz;
+		this ->N = N;
+		this -> M = M;
 	}
 	
 	void mover_arriba();
@@ -42,39 +53,88 @@ class Mapa{
 		return matriz;
 	}*/
 
-	void imprimir_tablero(int N, int M){
+	void imprimirTablero(){
 		
-		for(int i = 1; i <= N; i++){
-			for(int j = 1; j <= M; j++){
+		for(int i = 0; i < this->N; i++){
+			for(int j = 0; j < this->M; j++){
 		
 				if (matriz[i][j] == Elementos::VACIO){
-					cout<<"."<<endl;
+					cout<<".";
 				}
 
 				if (matriz[i][j] == Elementos::JUGADOR){
-					cout<<"K"<<endl;
+					cout<<"K";
 				}
 
 				if (matriz[i][j] == Elementos::CAJA){
-					cout<<"C"<<endl;
+					cout<<"C";
 				}
 
 				if (matriz[i][j] == Elementos::PARED){
-					cout<<"X"<<endl;
+					cout<<"X";
 				}
 
 				if (matriz[i][j] == Elementos::FINAL){
-					cout<<"F"<<endl;
+					cout<<"F";
 				}
 
 			}
+			cout<<endl;
 		}
 	}	
 };
 
-int main(){
+void pruebaTab(){
+	
+		Mapa::Elementos** matriz = new Mapa::Elementos*[4];
 
+		for(int i  = 0; i < 4; i++){ matriz[i] = new Mapa::Elementos[5]; }
+
+		matriz[0][0] = Mapa::Elementos::PARED;
+		matriz[0][1] = Mapa::Elementos::PARED;
+		matriz[0][2] = Mapa::Elementos::PARED;
+		matriz[0][3] = Mapa::Elementos::PARED;
+		matriz[0][4] = Mapa::Elementos::PARED;
+
+		matriz[1][0] = Mapa::Elementos::PARED;
+		matriz[1][1] = Mapa::Elementos::JUGADOR;
+		matriz[1][2] = Mapa::Elementos::VACIO;
+		matriz[1][3] = Mapa::Elementos::VACIO;
+		matriz[1][4] = Mapa::Elementos::PARED;
+
+		matriz[2][0] = Mapa::Elementos::PARED;
+		matriz[2][1] = Mapa::Elementos::VACIO;
+		matriz[2][2] = Mapa::Elementos::CAJA;
+		matriz[2][3] = Mapa::Elementos::FINAL;
+		matriz[2][4] = Mapa::Elementos::PARED;
+
+		matriz[3][0] = Mapa::Elementos::PARED;
+		matriz[3][1] = Mapa::Elementos::PARED;
+		matriz[3][2] = Mapa::Elementos::PARED;
+		matriz[3][3] = Mapa::Elementos::PARED;
+		matriz[3][4] = Mapa::Elementos::PARED;
+
+		//Iniciando en una pos random
+
+		Mapa map{1,1, 1,1, 4,4, matriz, 4,5};
+
+		map.imprimirTablero();
+	
+}
+	
+	/*
+		x x x x x		pared pared pared pared pared
+		x K	. .	x		pared jug vacio vacio pared
+		x . c f	x		pared vacio caja final pared
+		x x x x x 		pared pared pared pared pared
+	*/	
+
+
+int main(){
+	//int N, M;
 	Mapa map;			//Instancie mapa
-	map.imprimir_tablero(N, M);
+	
+	pruebaTab();
+
 	return 0;
 }
