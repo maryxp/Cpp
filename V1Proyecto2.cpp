19f0640d60d5a0623c386aef2356d1d300fcedce
Maria Laura <mary.cool2707@gmail.com>
2023-05-09 23:28:45 -0400

    funcion lectura de matriz en archivos

--- V1Proyecto2.cpp
+++ V1Proyecto2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<fstream>
 using namespace std;
 
 class Mapa{
@@ -10,10 +11,8 @@ class Mapa{
 	private: 
 	int posJugador_x;
 	int posJugador_y;
-	int posInicial_x;
-	int posInicial_y;
-	int posFinal_x;
-	int posFinal_y;
+	int puntoFinal_x;
+	int puntoFinal_y;
 	
 	public:
 	int N, M;
@@ -22,18 +21,14 @@ class Mapa{
 	Elementos** matriz;			//Para que se pueda usar la matriz por otras funciones
 	//La matriz posse elementos. Elementos es un tipo. La matriz es de elementos
 
-	Mapa(){
-		
-	}
+	Mapa(){}
 
-	Mapa(int posJugador_x, int posJugador_y, int posInicial_x, int posInicial_y, int posFinal_x, int posFinal_y, Elementos** matriz, int N, int M){
+	Mapa(int posJugador_x, int posJugador_y, int puntoFinal_x, int puntoFinal_y, Elementos** matriz, int N, int M){
 			
 		this->posJugador_x = posJugador_x;
 		this->posJugador_y = posJugador_y;
-		this->posInicial_x = posInicial_x;
-		this->posInicial_y = posInicial_y;
-		this->posFinal_x = posFinal_x;
-		this->posFinal_y = posFinal_y;
+		this->puntoFinal_x = puntoFinal_x;
+		this->puntoFinal_y = puntoFinal_y;
 		this ->matriz = matriz;
 		this ->N = N;
 		this -> M = M;
@@ -67,7 +62,6 @@ class Mapa{
 		
 	}
 
-
 	void mover_izquierda(){
 		Elementos aux = matriz[posJugador_y][posJugador_x];
 		Elementos posDecrementIzq = matriz[posJugador_y][posJugador_x-1];
@@ -95,7 +89,6 @@ class Mapa{
 		}
 	}
 
-	
 	void mover_abajo(){
 		Elementos aux = matriz[posJugador_y][posJugador_x];
 		Elementos posIncrement = matriz[posJugador_y +1][posJugador_x];
@@ -122,7 +115,6 @@ class Mapa{
 			matriz[posJugador_y+1][posJugador_x] = aux; 
 		}
 	}
-
 
 	void mover_derecha(){
 		Elementos aux = matriz[posJugador_y][posJugador_x];
@@ -194,6 +186,92 @@ class Mapa{
 	}	
 };
 
+class IOHandler{
+	Mapa mapaPrueba;
+	public:
+
+	IOHandler(){}
+	IOHandler(Mapa atributo, string nombreArchivo){ this->mapaPrueba = mapaPrueba; }
+
+	int leerArchivo(string nombreArchivo){
+		//Mapa map2;		
+		char caracter;
+		int N, M;
+		//string nombreArchivo;
+		//Para usarlas al crear el mapa		Seran igual a la pos del jugador en i j
+		int posJugador_x;	
+		int posJugador_y;
+		int puntoFinal_x;
+		int puntoFinal_y;
+	
+		ifstream f(nombreArchivo);
+
+		if(f.is_open()){		//Sin .eof ya que luego de leer a matriz seria lo ultimo que haya en el archivo
+			
+				f>>N;
+				f>>M;
+				
+				Mapa::Elementos** matriz = new  Mapa::Elementos*[N];
+
+				for(int i  = 0; i < N; i++){ matriz[i] = new Mapa::Elementos[M]; }
+
+					for (int i = 0; i < N; i++){
+						for (int j = 0; j < M; j++){
+							f>>caracter;
+
+							switch (caracter)
+							{
+								case 'X':
+									matriz[i][j] = Mapa::Elementos::PARED;
+									break;
+								
+								case 'C':
+									matriz[i][j] = Mapa::Elementos::CAJA;
+									break;
+								
+								case 'K':
+									matriz[i][j] = Mapa::Elementos::JUGADOR;
+										
+									posJugador_x = j;	//La altura, columna del jugador
+									posJugador_y = i;	//Que tan alejado esta del piso, fila 
+
+									break;
+								
+								case '.':
+									matriz[i][j] = Mapa::Elementos::VACIO;
+									break;
+								
+								case 'F':
+									matriz[i][j] = Mapa::Elementos::FINAL;
+									puntoFinal_x = j;
+									puntoFinal_y = i;
+									break;
+								
+								default:
+									return -1;	//Indicaria que hay error del que se encarga el menu
+									
+							}
+						}	
+					}
+			//los valores al crear el mapa con la matriz dentro del constructor no se saben
+			//porque en distintos archivos la matriz puede ser distinta por ende NO son constantes
+			
+			Mapa map2{posJugador_x, posJugador_y, puntoFinal_x,puntoFinal_y, matriz, N,M};
+			this->mapaPrueba = map2;		//Inicializar el atributo con los valores del constructor	
+		}
+	return 0;
+	}
+
+	void imprimirTablero(){
+
+		/*Como mapa es solo el capaz de imprimir, para que otras funciones puedan imprimir y
+		no romper el encapsulamiento  hay que crear un metodo que se llame igual y que invoque 
+		al imprimir original */
+		this->mapaPrueba.imprimirTablero();
+
+	}
+};
+
 void pruebaTab(){
 	
 		Mapa::Elementos** matriz = new Mapa::Elementos*[4];
@@ -226,25 +304,35 @@ void pruebaTab(){
 
 		//Iniciando en una pos random
 
-		Mapa map{1,1, 1,1, 4,4, matriz, 4,5};
+		Mapa map{1,1, 4,4, matriz, 4,5};
 
 		map.imprimirTablero();
-	
-}
-	
-	/*
+
+			/*
 		x x x x x		pared pared pared pared pared
 		x K	. .	x		pared jug vacio vacio pared
 		x . c f	x		pared vacio caja final pared
 		x x x x x 		pared pared pared pared pared
 	*/	
+	
+}
 
 
 int main(){
 	//int N, M;
 	Mapa map;			//Instancie mapa
-	
+	Mapa map2;
 	pruebaTab();
+
+	IOHandler lectura;
+
+	lectura.leerArchivo("mapaUno.txt");
+	lectura.imprimirTablero();		//Modifica el objeto luego que lee el archivo
+	lectura.leerArchivo("mapaDos.txt");
+	lectura.imprimirTablero();
+	lectura.leerArchivo("mapaTres.txt");
+	lectura.imprimirTablero();
+	
 
 	return 0;
 }
